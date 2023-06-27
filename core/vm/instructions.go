7061174d399f772927a25c4db85e08cee0ec2a34
Yulin <yulin970210@gmail.com>
2023-06-27 15:45:52 +0800

    fix:opCallCode 地址记录

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -721,6 +721,7 @@ func opCallCode(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([
 		bigVal = value.ToBig()
 	}
 	types.WriteStateKey(interpreter.evm.Context.BlockNumber, toAddr)
+	types.WriteTxCode(interpreter.evm.Context.BlockNumber, toAddr)
 	ret, returnGas, err := interpreter.evm.CallCode(scope.Contract, toAddr, args, gas, bigVal)
 	if err != nil {
 		temp.Clear()
